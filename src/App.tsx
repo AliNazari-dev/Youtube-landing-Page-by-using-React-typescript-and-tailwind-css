48b799638cae66f313076b7cd3a3c8e09b8153c6
Ali <AliNazarii7394@gmail.com>
2023-10-14 09:49:27 +0330

    project config

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import PageHeader from "./layout/PageHeader";
+
+const App = () => {
+  return (
+    <div className='max-h-screen flex flex-col'>
+      <PageHeader />
+      <div>2</div>
+    </div>
+  );
+};
+
+export default App;
